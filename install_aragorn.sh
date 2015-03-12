aa95921b2021355ec4512149c4cba41a628ef63b
Francisco Vina <fevb@kth.se>
2015-03-12 16:59:19 +0100

    added install script for aragorn

---
+++ install_aragorn.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source /opt/ros/hydro/setup.bash
+sudo apt-get install ros-hydro-moveit* 
+
+WS=~/amazon_challenge_ws
+
+# create catkin workspace
+# and install
+
+mdkir -p $WS/src
+cp amazon_challenge.rosinstall $WS/src/
+cd $WS/src
+
+catkin_init_workspace
+wstool init
+
+cd $WS
+catkin_make
+
+echo "source ~/amazon_challenge_ws/devel/setup.bash" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://pr2-c1:11311" >> ~/.bashrc
+echo "export ROS_IP=130.237.218.145" >> ~/.bashrc
+source ~/.bashrc
+
+cd $WS/src
+
+wstool merge amazon_challenge.rosinstall
+wstool update
+
+cd $WS
+catkin_make
+
+
+
+
