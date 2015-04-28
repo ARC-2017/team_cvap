1e41140216182ef08e8c1c221e7e75e7cc2562d0
Francisco Vina <fevb@kth.se>
2015-04-28 19:31:22 +0200

    added ros hydro installation

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+sudo apt-get install git emacs ssh
+
+# install ROS hydro
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get install ros-hydro-desktop-full
+
+# install ROS workspace
 
 source /opt/ros/hydro/setup.bash
 sudo apt-get install ros-hydro-moveit* ros-hydro-pr2* ros-hydro-gazebo* 
